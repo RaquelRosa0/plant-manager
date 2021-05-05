d8be5b385f7a299acae1e37c1a2b2a3fa084fd2b
raquel.rosa.costa@gmail.com <raquel.rosa.costa@gmail.com>
2021-05-05 18:41:42 -0300

    primeiro commit

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Apploading from 'expo-app-loading';
+import { UserIdentification } from './src/pages/UserIdentification';
+import {
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold
+} from '@expo-google-fonts/jost'
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold
+  });
+
+  if (!fontsLoaded)
+    return <Apploading />
+
+
+  return (
+    <UserIdentification />
+  )
+}
